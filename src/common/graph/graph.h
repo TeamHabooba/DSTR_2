b247b7091067b17c4d64306543339df411d15876
dedb0r0ded <revikad.00@gmail.com>
2026-05-28 00:06:58 +0800

    Copy array implementation from Assignment Part 1. Start the graph implementation.

--- src/common/graph/graph.h
+++ src/common/graph/graph.h
@@ -1,1 +1,32 @@
 #pragma once
+
+
+#include <common/aliases/aliases.h>
+#include <common/enums.h>
+#include <common/array/array.h>
+
+
+namespace dstr {
+
+  template<typename T, typename W>
+  class Link {
+    W weight_;
+    wp<Node<T, W>> target_;
+    
+  };
+
+
+  template<typename T, typename W>
+  class Node {
+    T value_;
+    Array<Link<T, W>> links_;
+  };
+
+  template<typename T, typename W = i32>
+  class Graph {
+    Array<sp<Node<T, W>>> container_;
+
+  };
+
+
+}
